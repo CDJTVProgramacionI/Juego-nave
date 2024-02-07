2fff49c7894acc16a8c363c08a95a5940386502f
unknown <dirm2705@gmail.com>
2024-02-07 16:10:21 -0600

    Cambios en el input del usuario

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "Frontend\format.h"
 #include <stdio.h>
+#include <conio.h>
 #include <time.h>
 
 void main()
@@ -11,7 +12,8 @@ void main()
     //La función time() nos permite que sea más aleatorio el número
     srand(time(0));
 
-    int op, vida, misiles, distancia, velocidad;
+    int vida, misiles, distancia, velocidad;
+    char op;
 
     /********************
     *      NIVEL 1      * 
@@ -27,42 +29,43 @@ void main()
     while(/*condicion para NO perder*/ 1 /*contador de tiempo u obstáculos*/)
     {
         //Generar una distancia aleatoria al objeto
-        //rand() % 8 genera un número entre 0 y 7
-        //al sumarle 5 se convierte en un número entre 5 y 12
-        distancia = (rand() % 8 + 5) * 1000;
-        velocidad = (rand() % 8 + 5) * 1000;
+
+        distancia = (rand() % 12 + 1) * 1000;
+        velocidad = (rand() % 15 + 1) * 1000;
 
         //Generar objeto u obstáculo al azar
-        int objeto = 0;//rand() % 3;
+        int objeto = rand() % 3;
 
         //Display
         printf(MAGENTA "Vida restante: %d \n",vida);
         printf(CYAN "Tu velocidad es: %d \n", velocidad);
-        printf(YELLOW "Misiles restantes: %d \n", misiles);	
+        printf(YELLOW "Misiles restantes: %d \n", misiles);
+
+        printf(RESET);	
         
 
         switch(objeto)
         {
             //Planeta sin vida
             case 0:
-                printf(RESET "Un " BLUE "PLANETA SIN VIDA " WHITE "se encuentra a %d km de distancia\n", distancia);
+                printf("Un " BLUE "PLANETA SIN VIDA " WHITE "se encuentra a %d km de distancia\n", distancia);
                 //Obtener input del usuario
                 do
                 {
                     printf("Que desea hacer?\n");
-                    printf("Presione 1 para esquivar y 2 para destruir\n");
-                    scanf("%d",&op);
+                    printf("Presione[E] para esquivar o [D] para destruir\n");
+                    op = getch();
 
                     switch (op)
                     {
                         //Decisiones para evitar o destruir obstáculos
-                        case 1:
+                        case 'E': case 'e':
                             if (distancia>=8000 && distancia< 10000 && velocidad >= 12000 || distancia >= 10000)
                                 printf("Se esquivo el planeta.\n");
                             else
                                 printf("Se ha estrellado con el planeta\n");
                             break;
-                        case 2: 
+                        case 'D': case 'd': 
                             if (distancia<=8000 && velocidad > 12000)
                             {
                                 printf("Se destruyo el planeta.\n");
@@ -77,66 +80,76 @@ void main()
                             }
                             break;
                         default:
-                            printf("No presiono una opcion correcta.");
+                            printf("No presiono una opcion correcta.\n");
                             break;
                     }
-                }while (op != 1 && op != 2);
+                }while (op != 'e' && op != 'E' && op != 'D' && op != 'd');
                 break;
             //Planeta con vida
             case 1:
                 printf("Un " BRIGHT_GREEN "PLANETA CON VIDA " WHITE "se encuentra a %d km de distancia\n", distancia);
                 //Obtener input del usuario
-                printf("Que desea hacer.\n");
-                printf("Presione 1 para esquivar y 2 para seguir adelante\n");
-                scanf("%d",&op);
-                switch (op)
+                do
                 {
-                    //Decisiones para evitar o destruir obstáculos
-                    case 1:
-                        if (distancia>=8000 && distancia< 10000 && velocidad > 12000 || distancia >= 10000)
-                            printf("Se esquivo el planeta.");
-                        else
-                            printf("Se ha estrellado con el planeta");
-                            vida -= 15;
-				            misiles -= 30;
-                        break;
-                    case 2: 
-                        if (distancia >= 10000 && distancia < 10000)
-                        {
-                            printf("Perdiste");
-                        }
-                        break;
-                    default:
-                        printf("No presiono una opcion correcta.");
-                        break;
-                }
+                    printf("Que desea hacer.\n");
+                    printf("Presione [E] para esquivar y [S] para seguir adelante\n");
+                    op = getch();
+
+                    switch (op)
+                    {
+                        //Decisiones para evitar o destruir obstáculos
+                        case 'e': case 'E':
+                            if (distancia>=8000 && distancia< 10000 && velocidad > 12000 || distancia >= 10000)
+                                printf("Se esquivo el planeta.\n");
+                            else
+                                printf("Se ha estrellado con el planeta\n");
+                            break;
+                        case 's': case 'S': 
+                            if (distancia < 8000)
+                            {
+                                printf("Perdiste");
+                            }
+                            break;
+                        default:
+                            printf("No presiono una opcion correcta.\n");
+                            break;
+                    }
+                } while (op != 'e' && op != 'E' && op != 'S' && op != 's');
+                
                 break;
             //Objeto de interés
             case 2:
                 printf("Un " MAGENTA "OBJETO DE INTERES " WHITE "se encuentra a %d km de distancia\n", distancia);
                 //Obtener input del usuario
                 printf("Que desea hacer.\n");
-                printf("Presione 1 para capturar y 2 para esquivar\n");
-                scanf("%d",&op);
-                switch (op)
+                printf("Presione [C] para capturar y [E] para esquivar\n");
+                op = getch();
+
+                do
                 {
-                    //Decisiones para capturar o esquivar
-                    case 1:
-                        if(distancia<=6000)
-                        {
-                            vida += 5;
-                        }
-                        else
-                        {
-                            vida -= 3;
-                        }
-                    break;
-                    case 2:
-                        printf("Esquivaste la capsula\n");
-                    default:
-                        printf("No presiono una opcion correcta.");
+                    switch (op)
+                    {
+                        //Decisiones para capturar o esquivar
+                        case 'c': case 'C':
+                            if(distancia <= 6000)
+                            {
+                                printf("Objeto capturado\n");
+                                vida += 5;
+                            }
+                            else
+                            {
+                                printf("Objeto no capturado\n");
+                                vida -= 3;
+                            }
                         break;
-                }
+                        case 'e': case 'E':
+                            printf("Esquivaste la capsula\n");
+                            break;
+                        default:
+                            printf("No presiono una opcion correcta.\n");
+                            break;
+                    }
+                } while (op != 'e' && op != 'E' && op != 'C' && op != 'c');
                 break;
         }
 
