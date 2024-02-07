80e0b5f7bc807d8c09d96990e594cd8716db228c
TaniaGarNi <159318034+TaniaGarNi@users.noreply.github.com>
2024-02-07 12:27:16 -0600

    yas.c
    Agregada la función de destruir o evitar objetos

--- main.c
+++ main.c
@@ -20,6 +20,38 @@ void main()
         //Obtener input del usuario
 
         //Decisiones para evitar o destruir obstáculos
+    int dist,op,vel;
+    printf("Solo tani escribe la velocidad:\n");
+    scanf("%d",& vel);
+    printf("Solo tani escribe la distancia: ");
+    scanf("%d",& dist);
+
+    if (dist>=10000 && vel>12000)
+    printf("Se esquivo el planeta.");
+    else if (dist>=8000 && dist<10000)
+    {
+     printf("Que desea hacer.\n");
+     printf("Presione 1 para Esquivar y 2 para destruir\n");
+     scanf("%d",&op);
+      switch (op)
+      {
+      case 1:
+        printf("Se esquivo el planeta.");break;
+      case 2: 
+        printf("Se destruyo el planeta.");break;
+      default:
+      printf("No presiono una opcion correcta.");break;
+      }
+    }
+     else if (dist<8000)
+     printf("Se ha estrellado con el planeta");
+     else if (dist<6000)
+     printf("Desea agarrar un objeto de interes");
+
+    
+    
+      
+
 
         //Decisiones para capsulas
     } 
